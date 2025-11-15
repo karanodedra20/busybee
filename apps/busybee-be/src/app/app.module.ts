46d34cc869299f7aa0e386a27e4ff9d0ee897b61
karanodedra20 <karanodedra81@gmail.com>
2025-11-15 15:34:25 +0530

    chore: add GraphQL support

--- apps/busybee-be/src/app/app.module.ts
+++ apps/busybee-be/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'apps/busybee-be/dist/schema.gql'),
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
